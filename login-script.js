1e3afcc425d687c10ad5f2bcbd92cbd07da11505
Ashish <ashish.binu1@gmail.com>
2021-09-19 16:47:31 +0530

    login v2

--- login-script.js
+++ login-script.js
@@ -1,30 +1,62 @@
 $(document).ready(() => {
-  //   $("#get").on("click", () => {
-  //     $.get("https://jsonplaceholder.typicode.com/todos", (data, stat) => {
-  //       console.log(data[0]);
-  //     });
-  //   });
+  //  $(".btn").on("click", validate(redirect));
 
-  $(".login").on("click", (e) => {
+  $(".btn").click(() => {
+    validate(redirect);
+  });
+
+  function validate(callback) {
+    var check = true;
     let uname = $(".uname");
     let pass = $(".pass");
 
     if (uname[0].value != "admin") {
-      e.preventDefault();
       uname.css("border", "solid red 2px");
       $(".uname+.fas").css("visibility", "visible");
+      check = false;
     } else {
       uname.css("border", "none");
       $(".uname+.fas").css("visibility", "hidden");
     }
 
     if (pass[0].value != 12345) {
-      e.preventDefault();
       pass.css("border", "solid red 2px");
       $(".pass+.fas").css("visibility", "visible");
+      check = false;
     } else {
       pass.css("border", "none");
       $(".pass+.fas").css("visibility", "hidden");
     }
-  });
+
+    callback(check);
+  }
+
+  function redirect(condn) {
+    if (condn == true) {
+      window.location.href = "./list.html";
+    }
+  }
 });
+
+// (e) => {
+//   let uname = $(".uname");
+//   let pass = $(".pass");
+
+//   if (uname[0].value != "admin") {
+//     e.preventDefault();
+//     uname.css("border", "solid red 2px");
+//     $(".uname+.fas").css("visibility", "visible");
+//   } else {
+//     uname.css("border", "none");
+//     $(".uname+.fas").css("visibility", "hidden");
+//   }
+
+//   if (pass[0].value != 12345) {
+//     e.preventDefault();
+//     pass.css("border", "solid red 2px");
+//     $(".pass+.fas").css("visibility", "visible");
+//   } else {
+//     pass.css("border", "none");
+//     $(".pass+.fas").css("visibility", "hidden");
+//   }
+// };
